501c2783db020b2df7037f2fb5499a2fe08f3817
redolf250 <redolfkendrick@gmail.com>
2022-09-26 12:06:49 +0000

    initial commit

---
+++ backend/driver.py
@@ -0,0 +1,74 @@
+
+
+
+
+import sys
+import platform
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+from ui_launcher import Ui_MainWindow
+from ui_dashoboard import Ui_dashboard
+
+counter = 0
+
+class MainWindow(QMainWindow):
+    def __init__(self, **kwargs):
+        QMainWindow.__init__(self, **kwargs)
+        self.ui = Ui_dashboard()
+        self.ui.setupUi(self)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.oldPosition = self.pos()
+        
+    def mousePressEvent(self, event):
+        self.oldPosition = event.globalPos()
+
+    def mouseMoveEvent(self,event):
+        delta = QPoint(event.globalPos() - self.oldPosition)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPosition = event.globalPos()
+        
+
+class Splash_screen(QMainWindow):
+    def __init__(self, **kwargs):
+        QMainWindow.__init__(self, **kwargs)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 70))
+        self.ui.main.setGraphicsEffect(self.shadow)
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.progress)
+        self.timer.start(40)
+
+        self.show()
+
+    def progress(self):
+        global counter
+        self.ui.progressBar.setValue(counter)
+        if counter > 100:
+            self.timer.stop()
+            self.main = MainWindow()
+            self.main.show()
+            self.close()
+        counter +=1
+
+
+if __name__ == '__main__':
+    application = QApplication(sys.argv)
+    window = Splash_screen()  
+    sys.exit(application.exec_()) 
---
+++ backend/ui_dashoboard.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'dashoboardCsdZjl.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_dashboard(object):
+    def setupUi(self, dashboard):
+        if dashboard.objectName():
+            dashboard.setObjectName(u"dashboard")
+        dashboard.resize(1279, 817)
+        dashboard.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.centralwidget = QWidget(dashboard)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setStyleSheet(u"QFrame{\n"
+"	\n"
+"	background-color: rgb(36, 35, 34);\n"
+"}")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+
+        self.verticalLayout.addWidget(self.frame)
+
+        dashboard.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(dashboard)
+
+        QMetaObject.connectSlotsByName(dashboard)
+    # setupUi
+
+    def retranslateUi(self, dashboard):
+        dashboard.setWindowTitle(QCoreApplication.translate("dashboard", u"MainWindow", None))
+    # retranslateUi
+
