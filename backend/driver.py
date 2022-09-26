fd6f561581d05ec5c9e718e54e41acdc3c3d3efa
redolf250 <redolfkendrick@gmail.com>
2022-09-26 15:02:34 +0000

    dashboard controls designed and code functionality added

--- backend/driver.py
+++ backend/driver.py
@@ -1,6 +1,10 @@
-
-
-
+################################################################################
+##
+## BY: Asamani Redolf
+## PROJECT MADE WITH: Qt Designer and PySide2
+## V: 1.0.0
+##
+################################################################################
 
 import sys
 import platform
@@ -15,6 +19,8 @@ from PySide2.QtWidgets import *
 from ui_launcher import Ui_MainWindow
 from ui_dashoboard import Ui_dashboard
 
+## ==> GLOBALS
+GLOBAL_STATE = 0
 counter = 0
 
 class MainWindow(QMainWindow):
@@ -24,9 +30,40 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-
         self.oldPosition = self.pos()
-        
+        self.ui.btn_close.clicked.connect(self.close)
+        self.ui.btn_minimize.clicked.connect(self.showMinimized)
+        self.ui.btn_maximize.clicked.connect(self.maximize_restore)
+
+         ## ==> CREATE SIZE GRIP TO RESIZE WINDOW
+        # self.sizegrip = QSizeGrip(self.ui.centralwidget)
+        # self.sizegrip.setStyleSheet("QSizeGrip { width: 10px; height: 10px; margin: 5px } QSizeGrip:hover { background-color: rgb(50, 42, 94) }")
+        # self.sizegrip.setToolTip("Resize Window")
+    
+    def maximize_restore(self):
+        global GLOBAL_STATE
+        status = GLOBAL_STATE
+
+        # IF NOT MAXIMIZED
+        if status == 0:
+            self.showMaximized()
+
+            # SET GLOBAL TO 1
+            GLOBAL_STATE = 1
+
+            # IF MAXIMIZED REMOVE MARGINS AND BORDER RADIUS
+            self.ui.drop_shadow_layout.setContentsMargins(0, 0, 0, 0)
+            #self.ui.drop_shadow_layout.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(42, 44, 111, 255), stop:0.521368 rgba(28, 29, 73, 255)); border-radius: 0px;")
+            self.ui.btn_maximize.setToolTip("Restore")
+        else:
+            GLOBAL_STATE = 0
+            self.showNormal()
+            #self.resize(self.width()+1, self.height()+1)
+            self.ui.drop_shadow_layout.setContentsMargins(0, 0, 0, 0)
+            #self.ui.drop_shadow_layout.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(42, 44, 111, 255), stop:0.521368 rgba(28, 29, 73, 255)); border-radius: 0px;")
+            self.ui.btn_maximize.setToolTip("Maximize")
+
+
     def mousePressEvent(self, event):
         self.oldPosition = event.globalPos()
 
--- backend/ui_dashoboard.py
+++ backend/ui_dashoboard.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'dashoboardCsdZjl.ui'
+## Form generated from reading UI file 'dashoboardTGvUsa.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.14.1
 ##
@@ -24,20 +24,141 @@ class Ui_dashboard(object):
         dashboard.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.centralwidget = QWidget(dashboard)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setStyleSheet(u"")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setSpacing(0)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.frame = QFrame(self.centralwidget)
-        self.frame.setObjectName(u"frame")
-        self.frame.setStyleSheet(u"QFrame{\n"
-"	\n"
-"	background-color: rgb(36, 35, 34);\n"
-"}")
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
+        self.drop_shadow_layout = QFrame(self.centralwidget)
+        self.drop_shadow_layout.setObjectName(u"drop_shadow_layout")
+        self.drop_shadow_layout.setStyleSheet(u"")
+        self.drop_shadow_layout.setFrameShape(QFrame.NoFrame)
+        self.drop_shadow_layout.setFrameShadow(QFrame.Raised)
+        self.drop_shadow_layout.setLineWidth(0)
+        self.content_layout = QVBoxLayout(self.drop_shadow_layout)
+        self.content_layout.setSpacing(0)
+        self.content_layout.setObjectName(u"content_layout")
+        self.content_layout.setContentsMargins(0, 0, 0, 0)
+        self.title_bar = QFrame(self.drop_shadow_layout)
+        self.title_bar.setObjectName(u"title_bar")
+        self.title_bar.setMaximumSize(QSize(16777215, 45))
+        self.title_bar.setStyleSheet(u"\n"
+"background-color: rgb(80, 81, 96);")
+        self.title_bar.setFrameShape(QFrame.NoFrame)
+        self.title_bar.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.title_bar)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.other_fields = QFrame(self.title_bar)
+        self.other_fields.setObjectName(u"other_fields")
+        self.other_fields.setFrameShape(QFrame.NoFrame)
+        self.other_fields.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.other_fields)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.logo = QFrame(self.other_fields)
+        self.logo.setObjectName(u"logo")
+        self.logo.setMaximumSize(QSize(100, 16777215))
+        self.logo.setFrameShape(QFrame.NoFrame)
+        self.logo.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.logo)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.logo)
+        self.label.setObjectName(u"label")
+        font = QFont()
+        font.setFamily(u"Times New Roman")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setStyleSheet(u"color: rgb(60, 231, 195);")
+        self.label.setMargin(10)
 
-        self.verticalLayout.addWidget(self.frame)
+        self.verticalLayout_2.addWidget(self.label)
+
+
+        self.horizontalLayout_2.addWidget(self.logo)
+
+        self.options = QFrame(self.other_fields)
+        self.options.setObjectName(u"options")
+        self.options.setFrameShape(QFrame.NoFrame)
+        self.options.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout_2.addWidget(self.options)
+
+
+        self.horizontalLayout.addWidget(self.other_fields)
+
+        self.controls_frame = QFrame(self.title_bar)
+        self.controls_frame.setObjectName(u"controls_frame")
+        self.controls_frame.setMinimumSize(QSize(100, 0))
+        self.controls_frame.setMaximumSize(QSize(100, 16777215))
+        self.controls_frame.setFrameShape(QFrame.NoFrame)
+        self.controls_frame.setFrameShadow(QFrame.Raised)
+        self.btn_maximize = QPushButton(self.controls_frame)
+        self.btn_maximize.setObjectName(u"btn_maximize")
+        self.btn_maximize.setGeometry(QRect(40, 20, 16, 16))
+        self.btn_maximize.setMinimumSize(QSize(16, 16))
+        self.btn_maximize.setMaximumSize(QSize(17, 17))
+        self.btn_maximize.setStyleSheet(u"QPushButton{\n"
+"	\n"
+"	background-color: rgb(255, 170, 0);\n"
+"	border:none;\n"
+"	border-radius: 8px;\n"
+"}\n"
+"\n"
+"QPushButton:hover{	\n"
+"	background-color: rgba(255, 170, 0,150);\n"
+"	\n"
+"}")
+        self.btn_minimize = QPushButton(self.controls_frame)
+        self.btn_minimize.setObjectName(u"btn_minimize")
+        self.btn_minimize.setGeometry(QRect(10, 20, 16, 16))
+        self.btn_minimize.setMinimumSize(QSize(16, 16))
+        self.btn_minimize.setMaximumSize(QSize(17, 17))
+        self.btn_minimize.setStyleSheet(u"QPushButton{\n"
+"	background-color: rgb(85, 255, 127);\n"
+"	border:none;\n"
+"	border-radius: 8px;\n"
+"}\n"
+"\n"
+"QPushButton:hover{	\n"
+"	background-color: rgba(85, 255, 127,150);\n"
+"	\n"
+"}")
+        self.btn_close = QPushButton(self.controls_frame)
+        self.btn_close.setObjectName(u"btn_close")
+        self.btn_close.setGeometry(QRect(70, 20, 16, 16))
+        self.btn_close.setMinimumSize(QSize(16, 16))
+        self.btn_close.setMaximumSize(QSize(17, 17))
+        self.btn_close.setStyleSheet(u"QPushButton{\n"
+"	background-color: rgb(255, 0, 0);\n"
+"	border:none;\n"
+"	border-radius: 8px;\n"
+"}\n"
+"\n"
+"QPushButton:hover{	\n"
+"	background-color: rgba(255, 0, 0,150);\n"
+"	\n"
+"}")
+
+        self.horizontalLayout.addWidget(self.controls_frame)
+
+
+        self.content_layout.addWidget(self.title_bar)
+
+        self.content = QFrame(self.drop_shadow_layout)
+        self.content.setObjectName(u"content")
+        self.content.setStyleSheet(u"background-color: rgb(57, 61, 67);")
+        self.content.setFrameShape(QFrame.NoFrame)
+        self.content.setFrameShadow(QFrame.Raised)
+
+        self.content_layout.addWidget(self.content)
+
+
+        self.verticalLayout.addWidget(self.drop_shadow_layout)
 
         dashboard.setCentralWidget(self.centralwidget)
 
@@ -48,5 +169,9 @@ class Ui_dashboard(object):
 
     def retranslateUi(self, dashboard):
         dashboard.setWindowTitle(QCoreApplication.translate("dashboard", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("dashboard", u"iVision", None))
+        self.btn_maximize.setText("")
+        self.btn_minimize.setText("")
+        self.btn_close.setText("")
     # retranslateUi
 
