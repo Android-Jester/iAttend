e48147734c2d4d4e961b94a55b99bcfe2724eee8
redolf250 <redolfkendrick@gmail.com>
2022-11-07 15:05:35 +0000

    typo in release fixed

--- backend/camera_one/surveillance_camera_one.py
+++ backend/camera_one/surveillance_camera_one.py
@@ -140,7 +140,7 @@ class Surveilliance_One(QtWidgets.QDialog):
     
     def stop_webcam(self):    
         self.show_alert = AlertDialog()
-        self.show_alert.content("Hey! wait a second while system\release camera")  
+        self.show_alert.content("Hey! wait a second while system\nrelease camera")  
         self.show_alert.show()
         self.surveillance_one.camera_feeds.setPixmap(QPixmap())
         self.surveillance_one.camera_feeds.setAlignment(Qt.AlignCenter)
