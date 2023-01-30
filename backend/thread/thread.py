8cf544a3f4bf74f00ab20b845c75035b4bba2948
redolf250 <redolfkendrick@gmail.com>
2023-01-30 22:09:44 +0000

    class name changed to ImageThread

--- backend/thread/thread.py
+++ backend/thread/thread.py
@@ -2,9 +2,9 @@ from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 
-class CustomThread(QRunnable):
+class ImageThread(QRunnable):
     def __init__(self,function):
-        super(CustomThread,self).__init__()
+        super(ImageThread,self).__init__()
         self.function = function
 
     def run(self):
