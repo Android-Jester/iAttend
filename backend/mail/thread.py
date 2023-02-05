000c74dd6ad28e3d7cb57936fc22fa4aaa724d14
redolf250 <redolfkendrick@gmail.com>
2023-02-05 12:44:57 +0000

    new mail class added

--- backend/mail/thread.py
+++ backend/mail/thread.py
@@ -54,6 +54,27 @@ class QRCodeMailThread(QRunnable):
     def run(self):
         message = MIMEMultipart()
         message['from']= self.details[3]
+        message['to']= self.details[0]
+        message['subject']= self.details[1]
+        message.attach(MIMEText(self.mail_content,'plain'))
+        message.attach(MIMEImage(Path(self.file_path).read_bytes()))
+        with smtplib.SMTP(host='smtp.gmail.com', port=587) as server:
+            server.starttls()
+            server.login(self.details[2],self.details[4])
+            server.send_message(message)
+
+
+class BatchQRCodeMailThread(QRunnable):
+    def __init__(self,details:list,mail_content,file_path,receiver):
+        self.details = details
+        self.mail_content = mail_content
+        self.file_path = file_path
+        self.receiver = receiver
+        super().__init__()
+ 
+    def run(self):
+        message = MIMEMultipart()
+        message['from']= self.details[3]
         message['to']= self.receiver
         message['subject']= self.details[1]
         message.attach(MIMEText(self.mail_content,'plain'))
