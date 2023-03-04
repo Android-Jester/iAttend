258689bfddecdb5a11d350fdf42100bacd4276c6
redolf250 <redolfkendrick@gmail.com>
2023-03-04 11:25:33 +0000

    user model added

---
+++ backend/model/user.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class UserDetails():
+    firstname: str
+    lastname: str
+    reference: str
+    contact: str
+    role: str
+    address: str
+   
