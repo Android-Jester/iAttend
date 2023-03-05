f3b5b0596e879d0cfc4159a08c14bec2437a02d4
redolf250 <redolfkendrick@gmail.com>
2023-03-05 23:45:30 +0000

    updated

--- backend/packages/model.py
+++ backend/packages/model.py
@@ -1,3 +1,5 @@
 from model.student import Student
+from model.user import UserDetails
 from model.generate_code import Code
 from model.attendance import Attendance
+from model.authentication import LoginUser
--- backend/packages/system.py
+++ backend/packages/system.py
@@ -7,9 +7,6 @@ import time
 import shutil
 import requests
 import winsound
-import keyboard
 from pathlib import Path
 from time import strptime
-from datetime import datetime as dt
 from datetime import datetime 
-from pathlib import Path
