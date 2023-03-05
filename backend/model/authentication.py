db1d8c592ddfe594012053bfdecd6bd10ffd7298
redolf250 <redolfkendrick@gmail.com>
2023-03-05 23:42:37 +0000

    auth and session model class added

---
+++ backend/model/authentication.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
+class LoginUser():
+    reference: str
+    username: str
+    password: str
+
+   
---
+++ backend/model/session.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class UserDetails():
+    reference: str
+    username: str
+    date: str
+    login: str
+    logout: str
+    duration: str
+   
