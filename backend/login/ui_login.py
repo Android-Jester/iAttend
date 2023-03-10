a986601b169d53e929e9d4ed579669098b93f710
redolf250 <redolfkendrick@gmail.com>
2023-03-10 16:48:31 +0000

    all functionalities added

--- backend/login/ui_login.py
+++ backend/login/ui_login.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'loginfujQeJ.ui'
+## Form generated from reading UI file 'loginUvtEuJ.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.14.1
 ##
