36bed054a243d6469b20c5ed795c83bc3eecdd6d
redolf250 <redolfkendrick@gmail.com>
2023-03-10 16:57:53 +0000

    updated

--- backend/user_details/ui_user_details.py
+++ backend/user_details/ui_user_details.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'user_detailsRVCQYa.ui'
+## Form generated from reading UI file 'user_detailsukloUk.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.14.1
 ##
@@ -21,7 +21,7 @@ class Ui_Profile(object):
     def setupUi(self, Profile):
         if Profile.objectName():
             Profile.setObjectName(u"Profile")
-        Profile.resize(499, 552)
+        Profile.resize(499, 733)
         self.verticalLayout = QVBoxLayout(Profile)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(9, -1, -1, -1)
@@ -167,31 +167,15 @@ class Ui_Profile(object):
 "\n"
 "}")
         self.firstname.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
-        self.awards = QLabel(self.frame_4)
-        self.awards.setObjectName(u"awards")
-        self.awards.setGeometry(QRect(70, 350, 81, 41))
-        self.awards.setFont(font1)
-        self.awards.setStyleSheet(u"QLabel{\n"
-"	color: rgb(255, 255, 255);\n"
-"	background-color: rgb(45, 45, 45);\n"
-"	border-radius: 10px;\n"
-"}")
-        self.awards.setAlignment(Qt.AlignCenter)
-        self.label_6 = QLabel(self.frame_4)
-        self.label_6.setObjectName(u"label_6")
-        self.label_6.setGeometry(QRect(20, 350, 40, 40))
-        self.label_6.setMinimumSize(QSize(40, 0))
-        self.label_6.setMaximumSize(QSize(40, 16777215))
-        self.label_6.setPixmap(QPixmap(u":/icons/asset/award.svg"))
         self.label_7 = QLabel(self.frame_4)
         self.label_7.setObjectName(u"label_7")
-        self.label_7.setGeometry(QRect(170, 350, 40, 40))
+        self.label_7.setGeometry(QRect(180, 350, 40, 40))
         self.label_7.setMinimumSize(QSize(40, 0))
         self.label_7.setMaximumSize(QSize(40, 16777215))
         self.label_7.setPixmap(QPixmap(u":/icons/asset/tag.svg"))
         self.role = QLabel(self.frame_4)
         self.role.setObjectName(u"role")
-        self.role.setGeometry(QRect(210, 350, 91, 41))
+        self.role.setGeometry(QRect(220, 350, 91, 41))
         self.role.setFont(font1)
         self.role.setStyleSheet(u"QLabel{\n"
 "	color: rgb(255, 255, 255);\n"
@@ -200,7 +184,7 @@ class Ui_Profile(object):
 "	padding-left:10px;\n"
 "\n"
 "}")
-        self.role.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.role.setAlignment(Qt.AlignCenter)
         self.lastname = QLabel(self.frame_4)
         self.lastname.setObjectName(u"lastname")
         self.lastname.setGeometry(QRect(270, 170, 191, 41))
@@ -215,7 +199,7 @@ class Ui_Profile(object):
         self.lastname.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
         self.email = QLabel(self.frame_4)
         self.email.setObjectName(u"email")
-        self.email.setGeometry(QRect(20, 290, 281, 41))
+        self.email.setGeometry(QRect(20, 290, 301, 41))
         self.email.setFont(font1)
         self.email.setStyleSheet(u"QLabel{\n"
 "	color: rgb(255, 255, 255);\n"
@@ -264,30 +248,18 @@ class Ui_Profile(object):
 "	border-radius: 10px;\n"
 "}")
         self.duration.setAlignment(Qt.AlignCenter)
-        self.visits = QLabel(self.frame_4)
-        self.visits.setObjectName(u"visits")
-        self.visits.setGeometry(QRect(320, 290, 141, 41))
-        self.visits.setFont(font1)
-        self.visits.setStyleSheet(u"QLabel{\n"
+        self.status = QLabel(self.frame_4)
+        self.status.setObjectName(u"status")
+        self.status.setGeometry(QRect(20, 350, 151, 41))
+        self.status.setFont(font1)
+        self.status.setStyleSheet(u"QLabel{\n"
 "	color: rgb(255, 255, 255);\n"
 "	background-color: rgb(45, 45, 45);\n"
 "	border-radius: 10px;\n"
 "	padding-left:10px;\n"
 "\n"
 "}")
-        self.visits.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
-        self.program = QLabel(self.frame_4)
-        self.program.setObjectName(u"program")
-        self.program.setGeometry(QRect(20, 470, 441, 41))
-        self.program.setFont(font1)
-        self.program.setStyleSheet(u"QLabel{\n"
-"	color: rgb(255, 255, 255);\n"
-"	background-color: rgb(45, 45, 45);\n"
-"	border-radius: 10px;\n"
-"	padding-left:10px;\n"
-"\n"
-"}")
-        self.program.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.status.setAlignment(Qt.AlignCenter)
         self.firstname_2 = QLabel(self.frame_4)
         self.firstname_2.setObjectName(u"firstname_2")
         self.firstname_2.setGeometry(QRect(140, 0, 191, 31))
@@ -300,6 +272,171 @@ class Ui_Profile(object):
 "\n"
 "}")
         self.firstname_2.setAlignment(Qt.AlignCenter)
+        self.update_username = QLineEdit(self.frame_4)
+        self.update_username.setObjectName(u"update_username")
+        self.update_username.setGeometry(QRect(20, 530, 321, 47))
+        self.update_username.setMinimumSize(QSize(0, 47))
+        self.update_username.setMaximumSize(QSize(16777215, 45))
+        font2 = QFont()
+        font2.setPointSize(10)
+        self.update_username.setFont(font2)
+        self.update_username.setStyleSheet(u"QLineEdit{\n"
+"    background-color: rgb(45,45,45);\n"
+"	color: rgb(255, 255, 255);\n"
+"	border: 2px solid  rgb(35, 35, 35);\n"
+"	border-radius:10px;\n"
+"	padding-left: 8px;\n"
+"}\n"
+"\n"
+"QLineEdit:hover{\n"
+"	border:2px solid rgb(35, 35, 35);\n"
+"}\n"
+"\n"
+"QLineEdit:focus{\n"
+"	border:2px solid rgb(255, 255, 255);\n"
+"}")
+        self.update_username.setClearButtonEnabled(True)
+        self.update_password = QLineEdit(self.frame_4)
+        self.update_password.setObjectName(u"update_password")
+        self.update_password.setGeometry(QRect(20, 590, 321, 47))
+        self.update_password.setMinimumSize(QSize(0, 47))
+        self.update_password.setMaximumSize(QSize(16777215, 45))
+        self.update_password.setFont(font2)
+        self.update_password.setStyleSheet(u"QLineEdit{\n"
+"    background-color: rgb(45,45,45);\n"
+"	color: rgb(255, 255, 255);\n"
+"	border: 2px solid  rgb(35, 35, 35);\n"
+"	border-radius:10px;\n"
+"	padding-left: 8px;\n"
+"}\n"
+"\n"
+"QLineEdit:hover{\n"
+"	border:2px solid rgb(35, 35, 35);\n"
+"}\n"
+"\n"
+"QLineEdit:focus{\n"
+"	border:2px solid rgb(255, 255, 255);\n"
+"}")
+        self.update_password.setClearButtonEnabled(True)
+        self.btn_update_profile = QPushButton(self.frame_4)
+        self.btn_update_profile.setObjectName(u"btn_update_profile")
+        self.btn_update_profile.setGeometry(QRect(350, 530, 111, 45))
+        self.btn_update_profile.setMinimumSize(QSize(0, 45))
+        self.btn_update_profile.setMaximumSize(QSize(16777215, 45))
+        self.btn_update_profile.setFont(font2)
+        self.btn_update_profile.setStyleSheet(u"QPushButton{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border:none;\n"
+"	border-radius: 10px;\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"	border-radius: 15px;\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"	border: 2px solid rgb(255,255,255);	\n"
+"}")
+        icon = QIcon()
+        icon.addFile(u":/icons/asset/image.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_update_profile.setIcon(icon)
+        self.btn_update_profile.setIconSize(QSize(30, 30))
+        self.btn_update_profile.setFlat(True)
+        self.update_profile = QLineEdit(self.frame_4)
+        self.update_profile.setObjectName(u"update_profile")
+        self.update_profile.setGeometry(QRect(20, 470, 321, 45))
+        self.update_profile.setMinimumSize(QSize(0, 45))
+        self.update_profile.setMaximumSize(QSize(16777215, 45))
+        self.update_profile.setFont(font2)
+        self.update_profile.setStyleSheet(u"QLineEdit{\n"
+"    background-color: rgb(45,45,45);\n"
+"	color: rgb(255, 255, 255);\n"
+"	border: 2px solid  rgb(35, 35, 35);\n"
+"	border-radius:10px;\n"
+"	padding-left: 8px;\n"
+"}\n"
+"\n"
+"QLineEdit:hover{\n"
+"	border:2px solid rgb(35, 35, 35);\n"
+"}\n"
+"\n"
+"QLineEdit:focus{\n"
+"	border:2px solid rgb(255, 255, 255);\n"
+"}")
+        self.update_profile.setClearButtonEnabled(True)
+        self.btn_update_user = QPushButton(self.frame_4)
+        self.btn_update_user.setObjectName(u"btn_update_user")
+        self.btn_update_user.setGeometry(QRect(350, 590, 111, 47))
+        self.btn_update_user.setMinimumSize(QSize(0, 47))
+        self.btn_update_user.setFont(font2)
+        self.btn_update_user.setStyleSheet(u"QPushButton{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border:none;\n"
+"	border-radius: 10px;\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"	border-radius: 15px;\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"	border: 2px solid rgb(255,255,255);	\n"
+"}")
+        icon1 = QIcon()
+        icon1.addFile(u":/icons/asset/repeat.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_update_user.setIcon(icon1)
+        self.btn_update_user.setIconSize(QSize(30, 30))
+        self.btn_update_user.setFlat(True)
+        self.reference = QLabel(self.frame_4)
+        self.reference.setObjectName(u"reference")
+        self.reference.setGeometry(QRect(330, 290, 131, 41))
+        self.reference.setFont(font1)
+        self.reference.setStyleSheet(u"QLabel{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border-radius: 10px;\n"
+"	padding-left:10px;\n"
+"\n"
+"}")
+        self.reference.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.btn_browse = QPushButton(self.frame_4)
+        self.btn_browse.setObjectName(u"btn_browse")
+        self.btn_browse.setGeometry(QRect(350, 470, 111, 45))
+        self.btn_browse.setMinimumSize(QSize(0, 45))
+        self.btn_browse.setMaximumSize(QSize(16777215, 45))
+        self.btn_browse.setFont(font2)
+        self.btn_browse.setStyleSheet(u"QPushButton{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border:none;\n"
+"	border-radius: 10px;\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"	border-radius: 15px;\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"	border: 2px solid rgb(255,255,255);	\n"
+"}")
+        icon2 = QIcon()
+        icon2.addFile(u":/icons/asset/file.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_browse.setIcon(icon2)
+        self.btn_browse.setIconSize(QSize(30, 30))
+        self.btn_browse.setFlat(True)
+        self.notification = QLabel(self.frame_4)
+        self.notification.setObjectName(u"notification")
+        self.notification.setGeometry(QRect(20, 650, 441, 41))
+        self.notification.setFont(font)
+        self.notification.setStyleSheet(u"QLabel{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border-radius: 10px;\n"
+"	padding-left:10px;\n"
+"}")
+        self.notification.setAlignment(Qt.AlignCenter)
 
         self.verticalLayout_2.addWidget(self.frame_4)
 
@@ -322,11 +459,9 @@ class Ui_Profile(object):
         self.btn_maximize.setText("")
         self.btn_minimize.setText("")
         self.label_5.setText("")
-        self.middlename.setText(QCoreApplication.translate("Profile", u"Middlename", None))
+        self.middlename.setText(QCoreApplication.translate("Profile", u"Othername", None))
         self.contact.setText(QCoreApplication.translate("Profile", u"Contact", None))
         self.firstname.setText(QCoreApplication.translate("Profile", u"Firstname", None))
-        self.awards.setText(QCoreApplication.translate("Profile", u"Awards", None))
-        self.label_6.setText("")
         self.label_7.setText("")
         self.role.setText(QCoreApplication.translate("Profile", u"Role", None))
         self.lastname.setText(QCoreApplication.translate("Profile", u"Lastname", None))
@@ -335,8 +470,15 @@ class Ui_Profile(object):
         self.label_8.setText("")
         self.last_seen.setText(QCoreApplication.translate("Profile", u"Last seen", None))
         self.duration.setText(QCoreApplication.translate("Profile", u"Duration", None))
-        self.visits.setText(QCoreApplication.translate("Profile", u"Visits", None))
-        self.program.setText(QCoreApplication.translate("Profile", u"Department", None))
+        self.status.setText(QCoreApplication.translate("Profile", u"Status", None))
         self.firstname_2.setText(QCoreApplication.translate("Profile", u"Profile", None))
+        self.update_username.setPlaceholderText(QCoreApplication.translate("Profile", u"Username", None))
+        self.update_password.setPlaceholderText(QCoreApplication.translate("Profile", u"Password", None))
+        self.btn_update_profile.setText(QCoreApplication.translate("Profile", u"Update", None))
+        self.update_profile.setPlaceholderText(QCoreApplication.translate("Profile", u"Profile image path", None))
+        self.btn_update_user.setText(QCoreApplication.translate("Profile", u"Update", None))
+        self.reference.setText(QCoreApplication.translate("Profile", u"Reference", None))
+        self.btn_browse.setText(QCoreApplication.translate("Profile", u"Browse", None))
+        self.notification.setText(QCoreApplication.translate("Profile", u"Notification", None))
     # retranslateUi
 
