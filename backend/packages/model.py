4b5127785dc514e0ef50c8c87c89e2fa89173fe1
redolf250 <redolfkendrick@gmail.com>
2023-03-30 15:24:05 +0000

    student college model added

--- backend/packages/model.py
+++ backend/packages/model.py
@@ -1,6 +1,8 @@
+from model.college import College
 from model.session import Session
 from model.student import Student
 from model.user import UserDetails
 from model.generate_code import Code
 from model.attendance import Attendance
 from model.authentication import LoginUser
+
