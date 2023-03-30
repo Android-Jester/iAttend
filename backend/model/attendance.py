1d1b1da6c0d46f859e2b043ebbf08ba41cd82912
redolf250 <redolfkendrick@gmail.com>
2023-03-30 15:21:09 +0000

    class variables named according to ddl structure

--- backend/model/attendance.py
+++ backend/model/attendance.py
@@ -3,9 +3,15 @@ from dataclasses import dataclass
 
 @dataclass
 class Attendance():
-    st_reference:str
-    program:str
-    date:str
-    time_in:str
-    time_out:str
-    duration:str
+    student_reference: str
+    student_college: str
+    student_faculty: str
+    student_program: str
+    student_category: str
+    student_nationality: str
+    student_gender: str
+    student_disability: str
+    date_stamp: str
+    time_in: str
+    time_out: str
+    duration: str
