3dd9df9249ff6353ee8030f8801d373ce80351a1
redolf250 <redolfkendrick@gmail.com>
2023-04-27 17:16:05 +0000

    ploting function updated, hand-coded values now passed at runtime

--- backend/report/barchart/barchart.py
+++ backend/report/barchart/barchart.py
@@ -6,8 +6,8 @@ class Barchart(FigureCanvas):
     def __init__(self):
         pass
 
-    def bar_plot_single_view(self, y_value:list,x_labels:list,bar_width:int,title:str,y_label:str,x_label:str,colors:list):
-        self.fig, self.ax = plt.subplots(1, dpi=100, figsize=(10, 10),
+    def bar_plot_single_view(self, y_values:list,x_labels:list,bar_width:int,y_label:str,x_label:str,colors:list,title:str,area,dpi):
+        self.fig, self.ax = plt.subplots(1, dpi=dpi, figsize=(area, area),
         sharey = False, facecolor='#2d2d2d')
         super().__init__(self.fig)
         self.ax.set_facecolor('#2d2d2d')
@@ -15,13 +15,13 @@ class Barchart(FigureCanvas):
         self.ax.spines['right'].set_color('#2d2d2d')
         self.ax.spines['left'].set_color('white')
         self.ax.spines['bottom'].set_color('white')     
-        plt.bar(x_labels,y_value,width=bar_width,alpha=0.8,color=colors)    
-        plt.title(title, size= 20, family = 'Arial',pad=30, color='white')
+        plt.bar(x_labels,y_values,width=bar_width,alpha=0.8,color=colors)    
+        plt.title(title, size= 20, family = 'Arial',pad=20, color='white')
         plt.xlabel(x_label,color='white',size= 15,labelpad= 13)
         plt.ylabel(y_label,color='white',size= 15,labelpad= 13)
         plt.yticks(color='white',size= 12)
         plt.xticks(color='white',size= 12)
-        plt.savefig('C:\\ProgramData\\iAttend\\data\\reports\\visualize\\barchart.png',dpi = 300, edgecolor='none')
+        plt.savefig('C:\\ProgramData\\iAttend\\data\\reports\\visualize\\barchart.png',dpi = dpi, edgecolor='none')
 
     def save_chart(self,name):
         plt.savefig('C:\\ProgramData\\iAttend\\data\\reports\\barchart\\'+name+'.pdf',dpi = 1000, edgecolor='none')
