7e2ef890c3c9d6a5ba6a339dc8bbc165c319a8b7
redolf250 <redolfkendrick@gmail.com>
2023-04-27 17:15:27 +0000

    ploting function updated, hand-coded values now passed at runtime

--- backend/report/piechart/piechart.py
+++ backend/report/piechart/piechart.py
@@ -5,15 +5,16 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 class Piechart(FigureCanvas):
     def __init__(self):
         pass
-    def piechart(self,data:list,title:str):
-        self.fig, self.ax = plt.subplots(1, dpi=100, figsize=(10, 10),
+    def piechart(self,data:list,title:str,colors:list,startangle,area,dpi,labeldistance,pctdistance):
+        self.fig, self.ax = plt.subplots(1, dpi=100, figsize=(area, area),
         sharey = False, facecolor='#2d2d2d')
         super().__init__(self.fig)
-        plt.title(title, size= 15, family = 'Arial',pad=30, color='white')  
-        self.ax.pie(data[0],labels=data[1],autopct = '%1.1f%%', labeldistance = 1.1
-        , radius=0.4, startangle =90, pctdistance = 0.4,textprops={'color':"w",'fontsize':13})
+        plt.title(title, size= 15, family = 'Arial',pad=30, color='white') 
+        plt.xlabel(xlabel=dict(zip(data[0], data[1])),color='white',size= 15,labelpad= 25)
+        self.ax.pie(data[1],labels=data[0],autopct = '%1.1f%%', colors=colors, labeldistance = labeldistance
+        , radius=0.4, startangle =startangle, pctdistance = pctdistance,textprops={'color':"w",'fontsize':13})
         self.ax.axis('equal')
-        plt.savefig('C:\\ProgramData\\iAttend\\data\\reports\\visualize\\piechart.png',dpi = 300, edgecolor='none')
+        plt.savefig('C:\\ProgramData\\iAttend\\data\\reports\\visualize\\piechart.png',dpi = dpi, edgecolor='none')
 
     def save_chart(self,name):
         plt.savefig('C:\\ProgramData\\iAttend\\data\\reports\\piechart\\'+name+'.pdf',dpi = 1000, edgecolor='none')
