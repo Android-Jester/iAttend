5207a64b97e7157ec16a44df66442d076fac6b4d
redolf250 <redolfkendrick@gmail.com>
2023-05-03 14:17:53 +0000

    xlabel rmoved but added it again

--- backend/report/piechart/piechart.py
+++ backend/report/piechart/piechart.py
@@ -5,12 +5,12 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 class Piechart(FigureCanvas):
     def __init__(self):
         pass
-    def piechart(self,data:list,title:str,colors:list,startangle,area,dpi,labeldistance,pctdistance):
+    def piechart(self,data:list,title:str,colors:list,startangle,area,dpi,labeldistance,pctdistance,xlabel):
         self.fig, self.ax = plt.subplots(1, dpi=100, figsize=(area, area),
         sharey = False, facecolor='#2d2d2d')
         super().__init__(self.fig)
-        plt.title(title, size= 15, family = 'Arial',pad=30, color='white') 
-        plt.xlabel(xlabel=dict(zip(data[0], data[1])),color='white',size= 15,labelpad= 25)
+        plt.title(title, size= 15, family = 'Arial',pad=30, color='white')
+        plt.xlabel(xlabel, size= 15, family = 'Arial',labelpad=30, color='white') 
         self.ax.pie(data[1],labels=data[0],autopct = '%1.1f%%', colors=colors, labeldistance = labeldistance
         , radius=0.4, startangle =startangle, pctdistance = pctdistance,textprops={'color':"w",'fontsize':13})
         self.ax.axis('equal')
