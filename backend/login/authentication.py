d2c736495910d8ac373cd74e358725c5cbf47806
redolf250 <redolfkendrick@gmail.com>
2023-05-08 13:33:00 +0000

    some commented codes deleted

--- backend/login/authentication.py
+++ backend/login/authentication.py
@@ -12,23 +12,10 @@ class Authentication(QMainWindow):
         self.ui_login.btn_minimize.clicked.connect(self.showMinimized)
         self.ui_login.btn_close.clicked.connect(self.close)
         self.ui_login.avater.setDisabled(True)
-        # self.ui_alert.frame.mouseMoveEvent = self.MoveWindow
 
-        # self.shadow = QGraphicsDropShadowEffect(self)
-        # self.shadow.setBlurRadius(20)
-        # self.shadow.setXOffset(0)
-        # self.shadow.setYOffset(0)
-        # self.shadow.setColor(QColor(230, 230, 230, 50))
-        # self.ui_alert.frame.setGraphicsEffect(self.shadow)
-    
-    # def MoveWindow(self, event):
-    #     if self.isMaximized() == False:
-    #         self.move(self.pos() + event.globalPos() - self.clickPosition)
-    #         self.clickPosition = event.globalPos()
-    #         event.accept()
-
-    # def mousePressEvent(self, event):
-    #     self.clickPosition = event.globalPos()
-
-    # def content(self, content: str):
-    #     return self.ui_alert.content.setText(content)
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(230, 230, 230, 50))
+        self.ui_login.frame.setGraphicsEffect(self.shadow)
