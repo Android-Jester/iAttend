989c9d3db60de6557bed537c69e55ddd5c174abe
redolf250 <redolfkendrick@gmail.com>
2023-05-24 21:47:03 +0000

    function to update temporal json file after scan and exiting user section

--- backend/utils/update.py
+++ backend/utils/update.py
@@ -10,3 +10,25 @@ def update_mail_details(path:str,properties):
         with open(path,'w') as content:
             json.dump(data, content, indent=4)
         content.close()
+
+def update_information_file():
+    path = 'C:\\ProgramData\\iAttend\\data\\student\\information.json'
+    with open(path,'r') as content:
+        update = json.load(content) 
+        update['firstname'] = 'firstname'
+        update['othername'] = 'othername'
+        update['lastname'] = 'lastname'
+        update['reference'] = 'reference'
+        update['index'] = 'index'
+        update['nationality'] = 'nationality'
+        update['college'] = 'college'
+        update['type'] = 'type'
+        update['gender'] = 'gender'
+        update['disabled'] = 'disabled'
+        update['department'] = 'department'
+        update['faculty'] = 'faculty'
+        update['validity'] = 'validity'
+    content.close()
+    with open(path,'w') as content:
+        json.dump(update, content, indent=4)
+    content.close()
