7c1370494b5b42eb7265724a7a1ad919cd64871c
redolf250 <redolfkendrick@gmail.com>
2023-05-24 08:29:45 +0000

    RequestThread added

--- backend/packages/mail.py
+++ backend/packages/mail.py
@@ -4,3 +4,4 @@ from mail.send import SendThread
 from mail.user_mail import UserMailThread
 from mail.thread import QRCodeMailThread
 from mail.thread import BatchQRCodeMailThread
+from thread.request_thread import RequestThread
