c884b34e81e7e03569f65e1cc3fb4f3c4e293684
redolf250 <redolfkendrick@gmail.com>
2023-05-29 14:12:43 +0000

    updated

--- backend/packages/misc.py
+++ backend/packages/misc.py
@@ -4,5 +4,6 @@ from merge.database import CentralDatabase
 from data.generated_data import DataView
 from scan_devices.camera import ActiveCameras
 from settings.settings import Settings
+from httplogs.http_response import HTTPResponse
 from configuration.configuration import Configuration
 from restapi.connections.restapi_endpoints import RESTAPI
