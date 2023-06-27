9605ba7095657140f7ec3828d836062fe310dfda
redolf250 <redolfkendrick@gmail.com>
2023-06-27 17:11:12 +0000

    user account registration account fetch from rest api issue resolved

--- backend/driver.py
+++ backend/driver.py
@@ -984,8 +984,12 @@ class MainWindow(QMainWindow):
                 pass
     
     def set_load_user_text(self):
-        self.ui.btn_user_fetch.setText("Loading...")
-        self.ui.btn_user_fetch.setIcon(QIcon(u":/icons/asset/loader.svg"))
+        if self.ui.user_search.text():
+            self.ui.btn_user_fetch.setText("Loading...")
+            self.ui.btn_user_fetch.setIcon(QIcon(u":/icons/asset/loader.svg"))
+        else:
+            self.ui.btn_user_fetch.setText("Load User")
+            self.ui.btn_user_fetch.setIcon(QIcon(u":/icons/asset/download.svg"))
 
     def render_user_interface(self, details:json):
         self.ui.user_firstname.setText(details['firstname'])
@@ -998,21 +1002,21 @@ class MainWindow(QMainWindow):
         user=self.read_user_endpoints()
         reference=self.ui.user_search.text()
         url = str(user['details']).replace('reference',reference)
-        if self.validate_field('^[0-9]+$',reference):
+        if reference and self.validate_field('^[0-9]+$',reference):
             try:
                 request_body = requests.get(url)
                 student_data=request_body.json()
                 if request_body.status_code == 200:
                     self.render_user_interface(student_data)
-                    self.ui.btn_user_fetch.setText("Load")
+                    self.ui.btn_user_fetch.setText("Load User")
                     self.ui.btn_user_fetch.setIcon(QIcon(u":/icons/asset/download.svg"))
                 else:
-                    self.ui.btn_user_fetch.setText("Load")
+                    self.ui.btn_user_fetch.setText("Load User")
                     self.ui.btn_user_fetch.setIcon(QIcon(u":/icons/asset/download.svg"))
                     self.alert.content(str(student_data))
                     self.alert.show()
             except Exception as e:
-                self.ui.btn_user_fetch.setText("Load")
+                self.ui.btn_user_fetch.setText("Load User")
                 self.ui.btn_user_fetch.setIcon(QIcon(u":/icons/asset/download.svg"))
                 self.alert.content(str(e))
                 self.alert.show()
